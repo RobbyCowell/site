07f1bda63380ee6fa4555069d920764554bebce5
Robby <robby.cowell@gmail.com>
2021-10-17 12:43:20 -0700

    adding article 4

---
+++ src/articles/004-the-basics/004-the-basics-metadata.js
@@ -0,0 +1,12 @@
+const rootURL = 'https://robbycowell.com';
+const articlesURL = 'dispatches';
+
+const slug = '004-the-articles'
+
+const metadata = {
+  publishDate: new Date(Date.now()),
+  slug,
+  permalink: `${rootURL}/${articlesURL}/${slug}`
+};
+
+export default metadata;
---
+++ src/articles/004-the-basics/004-the-basics.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  import metadata from './004-the-articles-metadata.js';
+  import article from './004-the-articles.md';
+
+  export const data = metadata;
+</script>
+
+<div>
+  {@html article}
+</div>
